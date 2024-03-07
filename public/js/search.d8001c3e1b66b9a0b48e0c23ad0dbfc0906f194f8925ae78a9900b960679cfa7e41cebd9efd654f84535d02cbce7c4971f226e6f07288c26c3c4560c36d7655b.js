73dbe8bdbc9cb767d295e7d5b0641d7678ab58fb
zhuxiujuan28 <562873187@qq.com>
2024-03-07 09:55:20 +0800

    add 4.1
    Signed-off-by: zhuxiujuan28 <562873187@qq.com>

--- public/js/search.d8001c3e1b66b9a0b48e0c23ad0dbfc0906f194f8925ae78a9900b960679cfa7e41cebd9efd654f84535d02cbce7c4971f226e6f07288c26c3c4560c36d7655b.js
+++ public/js/search.d8001c3e1b66b9a0b48e0c23ad0dbfc0906f194f8925ae78a9900b960679cfa7e41cebd9efd654f84535d02cbce7c4971f226e6f07288c26c3c4560c36d7655b.js
@@ -93,7 +93,7 @@ function searchFromSearchData(query, $searchDomMap) {
 }
 
 function getSearchData(callback) {
-  $.get("/js/search/" + $('.navbar-nav-item .project-version').text().trim() + "/lunr.json?216f54432a34345d07bb677b6caeb0a0", callback);
+  $.get("/js/search/" + $('.navbar-nav-item .project-version').text().trim() + "/lunr.json?0cc3fff35826a481e4eb6b269342cd85", callback);
 }
 
 function insertResultToDom(query, $searchDomMap, searchData) {
