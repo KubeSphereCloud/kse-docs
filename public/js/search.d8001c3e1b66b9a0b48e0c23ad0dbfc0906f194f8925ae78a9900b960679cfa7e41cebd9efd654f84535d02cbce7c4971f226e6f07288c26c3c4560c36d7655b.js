4e21f76ab62b7ed1040ddb01be0fc5c88ffd15c8
zhuxiujuan28 <562873187@qq.com>
2025-10-22 09:53:44 +0800

    update collect-node-info.sh
    Signed-off-by: zhuxiujuan28 <562873187@qq.com>

--- public/js/search.d8001c3e1b66b9a0b48e0c23ad0dbfc0906f194f8925ae78a9900b960679cfa7e41cebd9efd654f84535d02cbce7c4971f226e6f07288c26c3c4560c36d7655b.js
+++ public/js/search.d8001c3e1b66b9a0b48e0c23ad0dbfc0906f194f8925ae78a9900b960679cfa7e41cebd9efd654f84535d02cbce7c4971f226e6f07288c26c3c4560c36d7655b.js
@@ -93,7 +93,7 @@ function searchFromSearchData(query, $searchDomMap) {
 }
 
 function getSearchData(callback) {
-  $.get("/js/search/" + $('.navbar-nav-item .project-version').text().trim() + "/lunr.json?11fdbd9f6e22fad6d01962ea8e51e16a", callback);
+  $.get("/js/search/" + $('.navbar-nav-item .project-version').text().trim() + "/lunr.json?bcb8bf47760ae3afa9d883bd9ca2f978", callback);
 }
 
 function insertResultToDom(query, $searchDomMap, searchData) {
